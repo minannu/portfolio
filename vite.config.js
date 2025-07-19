d0c49d7820a9fe21aadb1cf55c2544198303b7a4
minnau <minannu.2001@gmail.com>
2025-07-19 12:38:02 +0600

    Update index.html title, change deployment directory to 'dist', and switch from BrowserRouter to HashRouter in main.jsx

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build", // Change output directory to 'build'
-  },
   base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
 });
